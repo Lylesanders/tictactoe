fe7609ca9433a65bb252b7bd4e060eae1ee18c75
lyle Sanders <lylesanders66@yahoo.com>
2016-10-20 18:03:32 -0400

    added player

--- tictactoe.rb
+++ tictactoe.rb
@@ -10,9 +10,31 @@ puts "\n\n"
 
 end
 
+def main_loop(p)
+make_board(p)
+
+end
+
+def set_players
+	prompt = ">> "
+	puts "Player 1, what is your name?"
+	print prompt
+	player_1_name = gets.chomp
+	puts "OK: #{player_1_name} are you using X or O?"
+	print prompt
+	player1 = gets.chomp
+	player1.upcase!
+	if player1 == "X"
+		player2 ="O"
+	else
+		player2 = "X"
+	end
+end
+
+# initialized_game
+
 # def initialized_game
 	p = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 # end
-
-# initialized_game
-make_board(p)
+set_players
+main_loop(p)
