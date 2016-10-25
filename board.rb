854e17c8f360ad5a6c7170287dc4e58c946e7583
lyle Sanders <lylesanders66@yahoo.com>
2016-10-25 14:16:10 -0400

    using strange magic

--- board.rb
+++ board.rb
@@ -7,7 +7,7 @@
          @position_board = Array.new(9, " ")
        end
        def update(position, symbol) #this is how to space is updated
-           @position_board[position] = symbol
+           @position_board[position.to_i] = symbol
        end
 
 
@@ -18,11 +18,9 @@
     
 
     end
-
-    
-
+  
     def valid_space?(position)
-    	if position_board[position] =="X" || position_board == "O"
+    	if position_board[position.to_i] =="X" || position_board[position.to_i] == "O"
     		false
     	else
     		true
@@ -31,8 +29,15 @@
     end
 
     def valid_input?(position)
-    	 position >= 0 && position <=8
-
-
+    	 position.to_i >= 0 && position.to_i <= 8 && position =~ (/\d/)
     end
+
+    # def integer?(position)
+    # 	 if position =~ /^[0-9]/
+    # 	 	true
+    # 	 else
+    # 	 	false
+    # 	 end
+    # end
+
 end
--- test_board.rb
+++ test_board.rb
@@ -9,35 +9,37 @@ class TestBoard < Minitest::Test
 
     def test_update_board
        board = Board.new
-       board.update(1, "X")
+       board.update("1", "X")
        assert_equal([" ","X"," "," "," "," "," "," "," "], board.position_board)
     end
     def test_update_board_o
        board = Board.new
        board.position_board = ["","X"," "," "," "," "," "," "," "]
-       board.update(0, "O")
+       board.update("0", "O")
        assert_equal(["O","X"," "," "," "," "," "," "," "], board.position_board)
     end
 
     
 
-    def test_for_occupied_space_false
-    	board = Board.new
-       board.position_board = ["X","O"," "," "," "," "," "," "," "]
-       assert_equal(false, board.valid_space?(0))
-    end
-    def test_for_occupied_space_true
-    	board = Board.new
-       board.position_board = ["X","O"," "," "," "," "," "," "," "]
-       assert_equal(true, board.valid_space?(2))
-    end
+#     def test_for_occupied_space_false
+#     	board = Board.new
+#        board.position_board = ["X","O"," "," "," "," "," "," "," "]
+#        assert_equal(false, board.valid_space?("0"))
+#     end
+#     def test_for_occupied_space_true
+#     	board = Board.new
+#        board.position_board = ["X","O"," "," "," "," "," "," "," "]
+#        assert_equal(true, board.valid_space?("2"))
+#     end
 
     def test_for_invalid_input
     	board = Board.new
-    	board.position_board = ["X"," "," "," "," "," "," "," "," "]
-    	assert_equal(true, board.valid_input?(0))
+    	assert_equal(true, board.valid_input?("9"))
+    	assert_equal(false, board.valid_input?(11))
     end
 
-
-
-end
+#     def test_input_invalid
+#     	board = Board.new
+#     	assert_equal(false, board.valid_input?("a"))	
+#     end
+ end
