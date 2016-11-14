49de3c09b819ab072765ee980fe31eeaba35a7c3
lyle Sanders <lylesanders66@yahoo.com>
2016-11-14 11:41:41 -0500

    updating github 1

--- board.rb
+++ board.rb
@@ -7,7 +7,7 @@ class Board
 		@empty_square = "-"# unplaid square is empty
 		@board = Array.new(9,"")
 		@position_board = Array.new(9,"")
-		@board = Board.new
+		#@board = Board.new
 
 	end #initialize
 
--- game_loop.rb
+++ game_loop.rb
@@ -8,6 +8,6 @@ game = ConsoleGame.new(Human.new("x"), Human.new("o"))
 
 game.print_board
 
-move = game.get_move(board) # from console_human det_move(board)
+ move = game.get_move #(board) # from console_human det_move(board)
 
 # game.make_move(move)
