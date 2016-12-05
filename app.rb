055d97b815c2574fcfba5ea65f2296c7e3279db1
lyle Sanders <lylesanders66@yahoo.com>
2016-12-05 14:21:47 -0500

    working as a web page

--- app.rb
+++ app.rb
@@ -14,6 +14,7 @@ ai = ""
 
 get '/' do
     session[:board] = Board.new #(["","","","","","","","",""])
+   
     erb :home, :layout => :home_layout, :locals =>{ :board => session[:board].board_positions}
 end #get '/' do
 
@@ -28,3 +29,72 @@ post '/player_1_name' do
     session[:current_player_name] = session[:player_1_name]
     erb :opponent, :layout => :home_layout, :locals =>{ :board => session[:board].board_positions, :player_1_name => session[:player_1_name] }
 end
+
+
+
+
+post '/choose_opponent' do
+    player_2 = params[:player_2]
+
+    if player_2 =="human"
+        session[:player_2] = Console_human.new("O")
+
+        erb :player_2_name, :layout => :home_layout, :locals =>{ :board => session[:board].board_positions}
+        session[:player_2_name] = session[:player_2]
+
+        redirect '/player_2_name'
+
+    elsif player_2 =="sequential_ai"
+        session[:player_2] = SequentialAi.new("O")
+        session[:player_2_name] = "Easy"
+
+        redirect '/get_move'
+
+    elsif player_2 =="random_ai"
+        session[:player_2] = RandomAi.new("O")
+        session[:player_2_name] = "Medium"
+
+        redirect '/get_move'
+
+    elsif player_2 =="unbeatable_ai"
+        session[:player_2] = UnbeatableAi.new("O")
+        session[:player_2_name] = "Hard"
+
+        redirect '/get_move'
+    end
+end
+
+get '/player_2_name' do
+    erb :player_2_name, :layout => :home_layout, :locals =>{ :board => session[:board].board_positions, :player_2_name => session[:player_2_name] }
+end
+
+post '/player_2_name' do
+    session[:player_2_name] = params[:player_2]
+    redirect '/get_move'
+
+end
+
+get '/get_move' do
+    move = session[:current_player].get_move(session[:board].board)
+
+    if move == "NO"
+    erb :get_move, :locals =>{ :current_player =>session[:current_player], :current_player_name => session[:current_player_name], :board => session[:board].board_positions }
+
+    elsif session[:board].valid_space?(move)
+        redirect 'make_move?move=' +move.to_s
+        else
+            redirect '/get_move'
+        end # if
+            end
+    
+    post '/get_player_move' do
+    move = params[:square].to_i
+
+    if session[:board].valid_space?(move)
+        redirect'/make_move?move=' + move.to_s
+
+        else
+            redirect '/get_move'
+        end
+
+    end
--- new_board.rb
+++ new_board.rb
@@ -1,53 +1,53 @@
 class Board
-attr_accessor :board
+
+attr_accessor :grid
 
     def initialize(board)
-        @board = board
-    end #initialize
+		@grid = board
+	end
 
-    def board_positions()
-        new_board = []
-        board.each_with_index do |value, index|
-            if value == "X" || value == "O"
-                new_board.push(value)
-            else
-                new_board.push(index + 1)
-            end #if
-        end # do loop
-        new_board
-    end # def board_positions
+	def board_positions()
+		new_board = []
+		grid.each_with_index do |value, index|
+			if value == "X" || value == "O"
+				new_board.push(value)
+			else
+				new_board.push(index + 1)
+			end
+		end
+		new_board
+	end
 
-    def update(position, marker)
-        board[position] = marker
-    end # update
+	def update(position, marker)
+		grid[position] = marker
+	end
 
-    def valid_space?(position)
-        position = (position - 1)
-        position >= 0 && position <= 8 && grid[position] == ""
-    end # valid space
+	def valid_space?(position)
+		position = (position - 1)
+		position >= 0 && position <= 8 && grid[position] == ""
+	end
 
-    def full_board?
-        grid.count("") == 0
-    end # full board
+	def full_board?
+		grid.count("") == 0
+	end
 
-    def winner?(marker)
-        result = false
-        winning_combo = [[grid[0],grid[1],grid[2]],
+	def winner?(marker)
+		result = false
+		winning_combo = [[grid[0],grid[1],grid[2]],
                          [grid[3],grid[4],grid[5]],
                          [grid[6],grid[7],grid[8]],
                          [grid[0],grid[3],grid[6]],
                          [grid[1],grid[4],grid[7]],
                          [grid[2],grid[5],grid[8]],
                          [grid[0],grid[4],grid[8]],
-                         [grid[2],grid[4],grid[6]]]  
+                         [grid[2],grid[4],grid[6]]]
 
         winning_combo.each do |winner|
-            if winner.count(marker) == 3
-                result = true
-            end # if
-        end # do loop
+        	if winner.count(marker) == 3
+        		result = true
+        	end
+        end
         result
-    end # winner
+	end
 
-
-end # class board
+end
